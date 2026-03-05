a0303e1e96a1fca9e37c65f5deb644dedb2f4656
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-05 14:05:16 +0100

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="talentapp:project_verification"
+          content="46eb454e1db771cea6ca98759164863b4bad221b308752ca9dd5e788e964bfd2c9d9bcb8297f24e00cbc99502c4f3f98017d7b4e45da414c22613d997604eb93"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

088a1d01ed98770472f46bf8e5e9e29228bde75c
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-05 13:03:17 +0100

    Create layout.js for Stacks March Lottery app

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Stacks March Lottery",
+  description: "Lottery app on Stacks",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}

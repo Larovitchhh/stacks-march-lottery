f74153017973e3f4f4c5def9b8edb66409b60d2f
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-05 13:07:41 +0100

    Add MintTicket component for minting tickets

---
+++ app/components/MintTicket.js
@@ -0,0 +1,40 @@
+"use client";
+
+import { openContractCall } from "@stacks/connect";
+import { StacksMainnet } from "@stacks/network";
+
+const network = new StacksMainnet();
+
+export default function MintTicket() {
+
+  const mint = async () => {
+
+    const txOptions = {
+      contractAddress: "TU_CONTRACT_ADDRESS",
+      contractName: "TU_CONTRACT_NAME",
+      functionName: "mint",
+      functionArgs: [],
+      network,
+      appDetails: {
+        name: "Stacks Lottery",
+      },
+    };
+
+    await openContractCall(txOptions);
+  };
+
+  return (
+    <button
+      onClick={mint}
+      style={{
+        marginTop:"20px",
+        padding:"12px 20px",
+        background:"green",
+        color:"white",
+        borderRadius:"8px"
+      }}
+    >
+      Mint Ticket
+    </button>
+  );
+}

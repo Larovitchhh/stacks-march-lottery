5b67e96c3faa706e67e08fcc1be35a1b298fe1a5
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-05 13:23:53 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+"use client";
+
+import { useState } from "react";
 import ConnectWallet from "./components/ConnectWallet";
 import MintTicket from "./components/MintTicket";
 
 export default function Home() {
+  const [wallet, setWallet] = useState(null);
+
   return (
     <main
       style={{
@@ -17,9 +22,9 @@ export default function Home() {
 
       <p>Mint a lottery ticket and receive a random number from 000 to 999</p>
 
-      <ConnectWallet />
+      <ConnectWallet setWallet={setWallet} />
 
-      <MintTicket />
+      {wallet && <MintTicket wallet={wallet} />}
     </main>
   );
 }

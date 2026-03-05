a384b3d4a43fbfc4790a01fa4416962f588c39c1
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-05 15:49:55 +0100

    Update head.js

--- app/head.js
+++ app/head.js
@@ -1,9 +1,12 @@
 // app/head.js
+export const metadata = {
+  title: "Stacks March Lottery",
+  description: "Lottery on Stacks blockchain",
+};
+
 export default function Head() {
   return (
     <>
-      <title>Stacks March Lottery</title>
-      <meta name="description" content="Lottery on Stacks blockchain" />
       <meta name="talentapp:project_verification" content="46eb454e1db771cea6ca98759164863b4bad221b308752ca9dd5e788e964bfd2c9d9bcb8297f24e00cbc99502c4f3f98017d7b4e45da414c22613d997604eb93" />
     </>
   );

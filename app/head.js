f497e0b3be2f46f9b3d1bc0d0c40879d2c2b7eed
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-05 15:46:49 +0100

    Update head.js

--- app/head.js
+++ app/head.js
@@ -1,17 +1,10 @@
+// app/head.js
 export default function Head() {
   return (
     <>
       <title>Stacks March Lottery</title>
-      <meta
-        name="description"
-        content="Participa en la Stacks March Lottery, mintea tu ticket y recibe un número aleatorio 000-999"
-      />
-      {/* Esto es lo que Talent Protocol va a verificar */}
-      <meta
-        name="talentapp:project_verification"
-        content="46eb454e1db771cea6ca98759164863b4bad221b308752ca9dd5e788e964bfd2c9d9bcb8297f24e00cbc99502c4f3f98017d7b4e45da414c22613d997604eb93"
-      />
-      <link rel="icon" href="/logo.png" />
+      <meta name="description" content="Lottery on Stacks blockchain" />
+      <meta name="talentapp:project_verification" content="46eb454e1db771cea6ca98759164863b4bad221b308752ca9dd5e788e964bfd2c9d9bcb8297f24e00cbc99502c4f3f98017d7b4e45da414c22613d997604eb93" />
     </>
   );
 }

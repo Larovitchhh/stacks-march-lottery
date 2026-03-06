19b0ff2c7f4da5d2e3071676207c794df7ac8b3c
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-06 08:41:11 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,15 @@
 export const metadata = {
   title: "Stacks March Lottery",
   description: "Lottery on Stacks blockchain",
+  other: {
+    "talentapp:project_verification":
+      "46eb454e1db771cea6ca98759164863b4bad221b308752ca9dd5e788e964bfd2c9d9bcb8297f24e00cbc99502c4f3f98017d7b4e45da414c22613d997604eb93",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
